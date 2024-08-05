eb58f0d9d934652695ae192844182c12f6152222
Mert Akkaya <76126526+mertaky8@users.noreply.github.com>
2024-08-05 19:37:29 +0300

    1.0.2

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,7 +1,6 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
-// Usage in a React Component
 const MyComponent = () => {
   interface Inputs {
     example: string;
@@ -16,7 +15,7 @@ const MyComponent = () => {
 
   const navigate = useNavigate();
   const onSubmit: SubmitHandler<Inputs> = (data) =>
-    data.exampleRequired === "123" && data.example === "Mert"
+    data.example === "Mert" && data.exampleRequired === "123"
       ? navigate("/form")
       : alert("Hatalı giriş yapıldı. Şifrenizi mi unuttunuz?");
 
@@ -25,14 +24,17 @@ const MyComponent = () => {
       {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* register your input into the hook by invoking the "register" function */}
-        <input placeholder="Kullanıcı Adı" {...register("example")} />
+        <input
+          placeholder="Kullanıcı Adı"
+          {...register("example", { required: true })}
+        />
         {/* include validation with required or other standard HTML validation rules */}
         <input
           placeholder="Şifre"
           {...register("exampleRequired", { required: true })}
         />
         {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
+        {errors.exampleRequired && <span>Bu alan doldurulmak zorunda!</span>}
         <input type="submit" />
       </form>
     </>
