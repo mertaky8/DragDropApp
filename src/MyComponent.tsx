49e1eb750a838fd62bc3a92ae20cbb1bf0a1f381
Mert Akkaya <76126526+mertaky8@users.noreply.github.com>
2024-08-06 01:32:24 +0300

    1.0.3

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -21,19 +21,15 @@ const MyComponent = () => {
 
   return (
     <>
-      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
       <form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
         <input
           placeholder="Kullanıcı Adı"
           {...register("example", { required: true })}
         />
-        {/* include validation with required or other standard HTML validation rules */}
         <input
           placeholder="Şifre"
           {...register("exampleRequired", { required: true })}
         />
-        {/* errors will return when field validation fails  */}
         {errors.exampleRequired && <span>Bu alan doldurulmak zorunda!</span>}
         <input type="submit" />
       </form>
